720f46b465007730e777e36698c7809a5b845347
Igor-A-Penkin <penkin.igor@gmail.com>
2020-09-29 15:38:56 +0300

    Empty project

---
+++ SocialApp/SocialApp/LoginFormViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginFormViewController.swift
+//  SocialApp
+//
+//  Created by Игорь Пенкин on 29.09.2020.
+//
+
+import UIKit
+
+class LoginFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
